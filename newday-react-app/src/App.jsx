b962eeac42d329b7d09ab0b83da350df2f7f44b6
cbscam-newday <cbscam@gmail.com>
2025-12-10 16:45:31 -0500

    Add files via upload

---
+++ newday-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', padding: '1.5rem' }}>
+      <h1>New Day Pest Control Service App</h1>
+      <p>React + Vite version is up and running.</p>
+      <p>Next we&apos;ll add: customers, jobs, calendar, materials, contracts, and receipts.</p>
+    </div>
+  )
+}
